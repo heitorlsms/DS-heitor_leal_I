efbe8bdcaca5b88b1e713ad3fcbf5f70491d955d
Heitor Leal <heitorlsms@gmail.com>
2024-04-08 09:23:06 -0300

    DS

---
+++ JavaApplication4/src/while/ex01.java
@@ -0,0 +1,11 @@
+package ifeswitch;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int V=1;
+        while(V<100){
+            V=V+2;
+            System.out.println("Impares:"+V);
+        }
+    }
+}
